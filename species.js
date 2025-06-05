dec0572c70627ff021cfef20d9f3af67f73eb19f
Cristian Iranzo <58115393+CristianICS@users.noreply.github.com>
2025-06-05 20:09:58 +0200

    Update and rename listado_especies.js to species.js

---
+++ species.js
@@ -0,0 +1,50 @@
+var species_metadata = [
+  {
+    "code": 1,
+    "name": "Acer",
+    "type": "genus",
+    "Commonly_known": "Maples"
+  },
+  {
+    "code": 2,
+    "name": "Betula",
+    "type": "genus",
+    "Commonly_known": "Birches"
+  },
+  {
+    "code": 3,
+    "name": "Populus",
+    "type": "genus",
+    "Commonly_known": "Poplars"
+  },
+  {
+    "code": 4,
+    "name": "Quercus",
+    "type": "genus",
+    "Commonly_known": "Oaks"
+  },
+  {
+    "code": 5,
+    "name": "Pinus",
+    "type": "genus",
+    "Commonly_known": "Pines"
+  },
+  {
+    "code": 6,
+    "name": "Picea",
+    "type": "genus",
+    "Commonly_known": "Spruces"
+  },
+  {
+    "code": 7,
+    "name": "Abies",
+    "type": "genus",
+    "Commonly_known": "Firs"
+  },
+  {
+    "code": 8,
+    "name": "Thuja",
+    "type": "genus",
+    "Commonly_known": "Cedars"
+  }
+]
