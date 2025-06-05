7a8bd9f2471bb0f6f54e9e0494abecce9b41d1fe
Cristian Iranzo <58115393+CristianICS@users.noreply.github.com>
2025-06-05 20:20:41 +0200

    Add customization JSON

---
+++ inventory_header.js
@@ -0,0 +1,30 @@
+var inv_header = {
+    "name": {
+        'custom_name': "Name",
+        'form_type': 'input',
+        'input_type': 'text',
+        'required': true,
+        'display_col': true
+    },
+    "central_point_id": {
+        'custom_name': "Central point ID",
+        'form_type': 'input',
+        'input_type': 'number',
+        'number_type': 'integer'
+    },
+    "crew_1": {
+        'custom_name': "Crew member 1",
+        'form_type': 'input',
+        'input_type': 'text'
+    },
+    "crew_2": {
+        'custom_name': "Crew member 2",
+        'form_type': 'input',
+        'input_type': 'text'
+    },
+    'comment': {
+        'custom_name': "Comments",
+        'description': 'Row comments.',
+        'form_type': 'textarea'
+    }
+}
