27bca0f92e11938d96bc25c98e4723f1db2d6d79
Cristian Iranzo <iranzocristian@gmail.com>
2025-08-22 08:23:52 +0200

    Organize files

---
+++ MIFCSheet/inventory_header.js
@@ -0,0 +1,26 @@
+var inv_header = {
+    "name": {
+        'custom_name': "Name",
+        'form_type': 'input',
+        'input_type': 'text',
+        'required': true,
+        'display_col': true
+    },
+    "init_point_id": {
+        'custom_name': "Initial point ID",
+        'form_type': 'input',
+        'input_type': 'number',
+        'number_type': 'integer'
+    },
+    "final_point_id": {
+        'custom_name': "Final point ID",
+        'form_type': 'input',
+        'input_type': 'number',
+        'number_type': 'integer'
+    },
+    'comment': {
+        'custom_name': "Comments",
+        'description': 'Row comments.',
+        'form_type': 'textarea'
+    }
+}
