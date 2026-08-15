5d9f2eb030cc95301f2b95698e6033997da62649
Cristian Iranzo <iranzocristian@gmail.com>
2026-08-15 20:58:35 +0200

    Update setupTests.js

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -6,23 +6,28 @@ globalThis.alert = vi.fn();
 
 function resetConfiguration() {
   globalThis.inv_header = {
-    plot_id: {
-      custom_name: "Plot ID",
-      form_type: "input",
-      input_type: "text",
-      required: true,
-      display_col: true
+    name: {
+        custom_name: "Name",
+        form_type: "input",
+        input_type: "text",
+        required: true,
+        display_col: true
     },
-    location: {
-      custom_name: "Location",
-      form_type: "input",
-      input_type: "text"
+      init_point_id: {
+        custom_name: "Initial point ID",
+        form_type: "input",
+        input_type: "number",
+        number_type: "integer"
     },
-    topography: {
-      custom_name: "Topography",
-      form_type: "select",
-      values: ["flat", "sloped"],
-      meanings: ["Flat", "Sloped"]
+      final_point_id: {
+        custom_name: "Final point ID",
+        form_type: "input",
+        input_type: "number",
+        number_type: "integer"
+    },
+      comment: {
+        custom_name: "Comments",
+        form_type: "textarea"
     }
   };
 
